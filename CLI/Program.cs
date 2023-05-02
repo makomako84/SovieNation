6f2799e3a713f6166c920692b99d65e735c7b4a6
hanzzbrr <artur.galimsky@gmail.com>
2023-05-02 10:33:28 +0600

    CLI client project

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,2 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var settings = context.Configuration;
+        services.Configure<ClientOptions>(settings.GetSection("Client"));
+        services.AddTransient<Client>();
+    })
+    .Build();
+
+var logger = host.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Client");
+
+CancellationTokenSource cancellationTokenSource = new();
+
+Console.CancelKeyPress += (sender, e) =>
+{
+    logger.LogInformation("cancellation initiated by the user");
+    cancellationTokenSource.Cancel();
+};
+
+var client = host.Services.GetRequiredService<Client>();
+await client.SendAndReceiveAsync(cancellationTokenSource.Token);
+
+
+record ClientOptions
+{
+    public string? Hostname { get; init; }
+    public int ServerPort { get; init; }
+}
+
+class Client
+{
+    private readonly string _hostname;
+    private readonly int _serverPort;
+    private readonly ILogger _logger;
+    public Client(IOptions<ClientOptions> options, ILogger<Client> logger)
+    {
+        _hostname = options.Value.Hostname ?? "localhost";
+        _serverPort = options.Value.ServerPort;
+        _logger = logger;
+    }
+
+    public async Task SendAndReceiveAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            Memory<byte> buffer = new byte[4096].AsMemory();
+            string? line;
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine(@"Press enter to read a quote, ""bye"" to exit");
+                line = Console.ReadLine();
+                if (line?.Equals("bye", StringComparison.CurrentCultureIgnoreCase) == true)
+                {
+                    repeat = false;
+                }
+                else
+                {
+                    TcpClient client = new();
+                    await client.ConnectAsync(_hostname, _serverPort, cancellationToken);
+                    using var stream = client.GetStream(); 
+                    int bytesRead = await stream.ReadAsync(buffer, cancellationToken);
+                    string quote = Encoding.UTF8.GetString(buffer.Span[..bytesRead]);
+                    buffer.Span[..bytesRead].Clear();
+                    Console.WriteLine(quote);
+                    Console.WriteLine();
+                }
+            };
+        }
+        catch (SocketException ex)
+        {
+            _logger.LogError(ex, ex.Message);
+        }
+
+        Console.WriteLine("so long, and thanks for all the fish");
+    }
+}
